b897c4f5ee1bd28c35a878cc9f11397eb093da84
김봉연 <bykim@angeluce.kr>
2022-08-18 17:33:00 +0900

    포토폴리오사이트 변경

--- src/views/project/PortolioSite.tsx
+++ src/views/project/PortolioSite.tsx
@@ -1,11 +1,24 @@
 import MainCard from "../../ui-component/cards/MainCard";
 import {Grid} from "@mui/material";
+import {useEffect, useState} from "react";
+import rest from "../../core/service/api.service";
+import {QuillEditorModel} from "../../core/models/quillEditor.model";
 
 
 function PortolioSite() {
-    return (
+    const [quillHtml, setQuillHtml]: any = useState('');
+
+    useEffect(() => {
+        rest.getHtml("portoliosite").then(res => {
+            const html: QuillEditorModel = res.data;
+            setQuillHtml(html.pageHtml);
+        })
+    }, []);
+
+    return(
         <MainCard title={"Project | PortolioSite"}>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6} className={"ql-editor"} dangerouslySetInnerHTML={{__html: quillHtml}}>
+
             </Grid>
         </MainCard>
     )

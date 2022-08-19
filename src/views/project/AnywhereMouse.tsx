cd80e014de8b442c361d4471fef6b463ac57001f
김봉연 <bykim@angeluce.kr>
2022-08-19 22:08:08 +0900

    애니웨어 특허출원

--- src/views/project/AnywhereMouse.tsx
+++ src/views/project/AnywhereMouse.tsx
@@ -3,6 +3,7 @@ import {Grid} from "@mui/material";
 import {useEffect, useState} from "react";
 import rest from "../../core/service/api.service";
 import {QuillEditorModel} from "../../core/models/quillEditor.model";
+import {Link} from "react-router-dom";
 
 function AnywhereMouse() {
     const [quillHtml, setQuillHtml]: any = useState('');
@@ -17,6 +18,7 @@ function AnywhereMouse() {
     return (
         <MainCard title={"AnywhereMouse"}>
             <Grid item xs={12} sm={6} className={"ql-editor"} dangerouslySetInnerHTML={{__html: quillHtml}}/>
+            <Link to={"/uploads/localdir/폰케이스형_블루투스_마우스_특허출원.pdf"}>ㄴㅇㅁㄴㅇ</Link>
         </MainCard>
     )
 }

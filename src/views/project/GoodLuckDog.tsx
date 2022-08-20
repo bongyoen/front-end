7c1340637d932f988b0598e8b132cafa17eddb43
김봉연 <bykim@angeluce.kr>
2022-08-21 00:04:22 +0900

    개팔자상팔자페이지 페이지로드 추가

--- src/views/project/GoodLuckDog.tsx
+++ src/views/project/GoodLuckDog.tsx
@@ -1,11 +1,23 @@
 import MainCard from "../../ui-component/cards/MainCard";
 import {Grid} from "@mui/material";
+import "../../core/css/introduce.scss";
+import {useEffect, useState} from "react";
+import rest from "../../core/service/api.service";
+import {QuillEditorModel} from "../../core/models/quillEditor.model";
 
 function GoodLuckDog() {
+    const [quillHtml, setQuillHtml]: any = useState('');
+
+    useEffect(() => {
+        rest.getHtml("goodluckdog").then(res => {
+            const html: QuillEditorModel = res.data;
+            setQuillHtml(html.pageHtml);
+        })
+    }, []);
+
     return (
         <MainCard title={"Project | GoodLuckDog"}>
-            <Grid item xs={12} sm={6}>
-            </Grid>
+            <Grid item xs={12} sm={6} className={"ql-editor"} dangerouslySetInnerHTML={{__html: quillHtml}}/>
         </MainCard>
     )
 }
